954294e56a5eb8343d07bde2db3fe9b17e56724b
Alex Averbuch <alex.averbuch@gmail.com>
2010-05-09 15:20:30 +0200

    ~ small cleanup
    Signed-off-by: Alex Averbuch <alex.averbuch@gmail.com>

--- src/infoDB/InstanceInfo.java
+++ src/infoDB/InstanceInfo.java
@@ -7,28 +7,28 @@ import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 
 public class InstanceInfo implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 	public HashMap<Long, Long> interHopMap;
-	private long[] accesses; 
-	
-	public enum InfoKey{
+	private long[] accesses;
+
+	public enum InfoKey {
 		InterHop, IntraHop, NumNodes, NumRelas, Traffic, rs_delete, n_delete, rs_create, n_create
 	}
-	
+
 	public InstanceInfo() {
 		this.interHopMap = new HashMap<Long, Long>();
-		this.accesses = new long[InfoKey.values().length] ;
-		for(int i=0; i<InfoKey.values().length ; i++){
-			accesses[i]=0;
+		this.accesses = new long[InfoKey.values().length];
+		for (int i = 0; i < InfoKey.values().length; i++) {
+			accesses[i] = 0;
 		}
 	}
-	
-	public long getValue(InfoKey key){
+
+	public Long getValue(InfoKey key) {
 		return accesses[key.ordinal()]++;
 	}
-	
-	public void log( InfoKey key ){
+
+	public void log(InfoKey key) {
 		accesses[key.ordinal()]++;
 		switch (key) {
 		case rs_create:
@@ -52,65 +52,65 @@ public class InstanceInfo implements Serializable {
 		}
 	}
 
-	public void logHop(Relationship rs){
-		
-		if(rs.hasProperty("_isGhost") || rs.hasProperty("_isHalf")){
+	public void logHop(Relationship rs) {
+
+		if (rs.hasProperty("_isGhost") || rs.hasProperty("_isHalf")) {
 			// interhop on partitioned db
 			accesses[InfoKey.InterHop.ordinal()]++;
 			return;
-		}			
-		
+		}
+
 		Node[] nodes = rs.getNodes();
-		if(nodes[0].hasProperty("_color")) {
+		if (nodes[0].hasProperty("_color")) {
 			byte c1 = (Byte) nodes[0].getProperty("_color");
 			byte c2 = (Byte) nodes[1].getProperty("_color");
-		
-			if(c1!=c2){
+
+			if (c1 != c2) {
 				accesses[InfoKey.InterHop.ordinal()]++;
 				return;
 			}
 		}
-			
+
 		// normal hop
 		accesses[InfoKey.IntraHop.ordinal()]++;
-		
+
 	}
-	
-	public void resetTraffic(){
+
+	public void resetTraffic() {
 		accesses[InfoKey.Traffic.ordinal()] = 0;
 		accesses[InfoKey.InterHop.ordinal()] = 0;
 		accesses[InfoKey.IntraHop.ordinal()] = 0;
 		this.interHopMap = new HashMap<Long, Long>();
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		String res = "{";
 		for (InfoKey k : InfoKey.values()) {
-			res+="("+k.name() +" = " + accesses[k.ordinal()]+ ") ";
+			res += "(" + k.name() + " = " + accesses[k.ordinal()] + ") ";
 		}
-		res+="}"+ interHopMap;
+		res += "}" + interHopMap;
 		return res;
 	}
-	
-	public InstanceInfo differenceTo(InstanceInfo info){
+
+	public InstanceInfo differenceTo(InstanceInfo info) {
 		InstanceInfo res = info.takeSnapshot();
-		for(long id: this.interHopMap.keySet()){
+		for (long id : this.interHopMap.keySet()) {
 			long val = this.interHopMap.get(id);
-			if(res.interHopMap.containsKey(id)){
+			if (res.interHopMap.containsKey(id)) {
 				val = res.interHopMap.get(id) - val;
-			}else{
+			} else {
 				val = -val;
 			}
 			res.interHopMap.put(id, val);
 		}
-		for(int i = 0; i < accesses.length; i++ ){
-			res.accesses[i] -=this.accesses[i];
+		for (int i = 0; i < accesses.length; i++) {
+			res.accesses[i] -= this.accesses[i];
 		}
 		return res;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public InstanceInfo takeSnapshot(){
+	public InstanceInfo takeSnapshot() {
 		InstanceInfo clone = new InstanceInfo();
 		clone.interHopMap = (HashMap<Long, Long>) this.interHopMap.clone();
 		clone.accesses = accesses.clone();
